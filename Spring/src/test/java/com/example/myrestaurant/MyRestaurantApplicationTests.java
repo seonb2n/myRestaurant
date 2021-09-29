60555dda92b599099736119a6f9b838663ea2e6d
seonb2n <626ksb@gmail.com>
2021-09-29 19:53:20 +0900

    spring server 생성

---
+++ Spring/src/test/java/com/example/myrestaurant/MyRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myrestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
