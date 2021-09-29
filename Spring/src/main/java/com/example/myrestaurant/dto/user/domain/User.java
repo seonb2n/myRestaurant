b60f5f5488db37b29ee251d4856c1d934b095c3e
seonb2n <626ksb@gmail.com>
2021-09-29 20:04:32 +0900

    spring server 기본적인 package 구조 설계

---
+++ Spring/src/main/java/com/example/myrestaurant/dto/user/domain/User.java
@@ -0,0 +1,29 @@
+package com.example.myrestaurant.dto.user.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name="user")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", foreignKey = @ForeignKey(name = "user_id"))
+    private Set<UserAuthority> authorities;
+
+
+}
---
+++ Spring/src/main/java/com/example/myrestaurant/dto/user/domain/UserAuthority.java
@@ -0,0 +1,26 @@
+package com.example.myrestaurant.dto.user.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name="user_authority")
+@IdClass(UserAuthority.class)
+public class UserAuthority implements GrantedAuthority {
+
+    @Id
+    @Column
+    private Long userId;
+
+    @Id
+    private String authority;
+}
