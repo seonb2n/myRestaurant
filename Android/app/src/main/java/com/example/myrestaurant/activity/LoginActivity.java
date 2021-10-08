9f6b9638db0f6883f4ee698123161f33a2ba2400
seonb2n <626ksb@gmail.com>
2021-10-08 20:00:20 +0900

    main activity 에서 사용자 위치 주변 맛집 검색하는 기능 추가

--- Android/app/src/main/java/com/example/myrestaurant/activity/LoginActivity.java
+++ Android/app/src/main/java/com/example/myrestaurant/activity/LoginActivity.java
@@ -6,9 +6,12 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -42,13 +45,15 @@ public class LoginActivity extends AppCompatActivity {
     EditText textViewPw;
 
     static final int INTERNET_PERMISSON=1;
+    private static final int GPS_ENABLE_REQUEST_CODE = 2001;
+    private static final int PERMISSIONS_REQUEST_CODE = 100;
+    String[] REQUIRED_PERMISSIONS = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+
 
     private Retrofit retrofit;
     private RetrofitService retrofitService;
     private final String BASEURL = "http://172.30.1.13:8833/";
 
-    static String loginSuccessId;
-    static String loginSuccessPw;
     private SharedPreferences auto;
 
     @Override
@@ -61,6 +66,24 @@ public class LoginActivity extends AppCompatActivity {
         textViewId = findViewById(R.id.textViewId);
         textViewPw = findViewById(R.id.textViewPw);
         auto = getSharedPreferences("auto", Activity.MODE_PRIVATE);
+
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASEURL)
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+        retrofitService = retrofit.create(RetrofitService.class);
+
+
+        if (!checkLocationServicesStatus()) {
+            showDialogForLocationServiceSetting();
+        } else {
+            checkRunTimePermission();
+        }
 
         int permissonCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET);
 
@@ -84,41 +107,10 @@ public class LoginActivity extends AppCompatActivity {
 
         String loginId = auto.getString("inputId", null);
         String loginPwd = auto.getString("inputPwd", null);
-        tryLogin(loginId, loginPwd);
+        if(loginId != null && loginPwd != null) {
+            tryLogin(loginId, loginPwd);
+        }
 
-    }
-
-    private void setRetrofitInit() {
-
-        Gson gson = new GsonBuilder()
-                .setLenient()
-                .create();
-
-        retrofit = new Retrofit.Builder()
-                .baseUrl(BASEURL)
-                .addConverterFactory(ScalarsConverterFactory.create())
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .build();
-        retrofitService = retrofit.create(RetrofitService.class);
-
-        Call<UserResult> call = retrofitService.getUser();
-
-        call.enqueue(new Callback<UserResult>() {
-            @Override
-            public void onResponse(Call<UserResult> call, Response<UserResult> response) {
-                if(response.isSuccessful()) {
-                    UserResult result = response.body();
-                    Log.d(TAG, "onResponse: 성공, 결과 \n"+result.toString());
-                } else {
-                    Log.d(TAG, "onResponse: 실패");
-                }
-            }
-
-            @Override
-            public void onFailure(Call<UserResult> call, Throwable t) {
-                Log.d(TAG, "onFailure: " + t.getMessage());
-            }
-        });
     }
 
     private void tryLogin(String id, String password) {
@@ -139,6 +131,7 @@ public class LoginActivity extends AppCompatActivity {
                     finish();
                 } else {
                     Log.d(TAG, "onResponse: 실패 \n" + response.body());
+                    Toast.makeText(LoginActivity.this, "재 로그인이 필요합니다.", Toast.LENGTH_SHORT).show();
                 }
             }
 
@@ -156,4 +149,80 @@ public class LoginActivity extends AppCompatActivity {
 
         tryLogin(id, password);
     }
+
+    //GPS 사용을 위한 메소드
+    public boolean checkLocationServicesStatus() {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    private void showDialogForLocationServiceSetting() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+        builder.setTitle("위치 서비스 비활성화");
+        builder.setMessage("앱을 사용하기 위해서는 위치 서비스가 필요합니다.\n"
+                + "위치 설정을 수정하실래요?");
+        builder.setCancelable(true);
+        builder.setPositiveButton("설정", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                Intent callGPSSettingIntent
+                        = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivityForResult(callGPSSettingIntent, GPS_ENABLE_REQUEST_CODE);
+            }
+        });
+        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+            }
+        });
+        builder.create().show();
+    }
+
+    void checkRunTimePermission() {
+
+        //런타임 퍼미션 처리
+        // 1. 위치 퍼미션을 가지고 있는지 체크합니다.
+        int hasFineLocationPermission = ContextCompat.checkSelfPermission(LoginActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        int hasCoarseLocationPermission = ContextCompat.checkSelfPermission(LoginActivity.this,
+                Manifest.permission.ACCESS_COARSE_LOCATION);
+
+
+        if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED &&
+                hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED) {
+
+            // 2. 이미 퍼미션을 가지고 있다면
+            // ( 안드로이드 6.0 이하 버전은 런타임 퍼미션이 필요없기 때문에 이미 허용된 걸로 인식합니다.)
+
+
+            // 3.  위치 값을 가져올 수 있음
+
+
+        } else {  //2. 퍼미션 요청을 허용한 적이 없다면 퍼미션 요청이 필요합니다. 2가지 경우(3-1, 4-1)가 있습니다.
+
+            // 3-1. 사용자가 퍼미션 거부를 한 적이 있는 경우에는
+            if (ActivityCompat.shouldShowRequestPermissionRationale(LoginActivity.this, REQUIRED_PERMISSIONS[0])) {
+
+                // 3-2. 요청을 진행하기 전에 사용자가에게 퍼미션이 필요한 이유를 설명해줄 필요가 있습니다.
+                Toast.makeText(LoginActivity.this, "이 앱을 실행하려면 위치 접근 권한이 필요합니다.", Toast.LENGTH_LONG).show();
+                // 3-3. 사용자게에 퍼미션 요청을 합니다. 요청 결과는 onRequestPermissionResult에서 수신됩니다.
+                ActivityCompat.requestPermissions(LoginActivity.this, REQUIRED_PERMISSIONS,
+                        PERMISSIONS_REQUEST_CODE);
+
+
+            } else {
+                // 4-1. 사용자가 퍼미션 거부를 한 적이 없는 경우에는 퍼미션 요청을 바로 합니다.
+                // 요청 결과는 onRequestPermissionResult에서 수신됩니다.
+                ActivityCompat.requestPermissions(LoginActivity.this, REQUIRED_PERMISSIONS,
+                        PERMISSIONS_REQUEST_CODE);
+            }
+
+        }
+
+    }
+
 }
--- Android/app/src/main/java/com/example/myrestaurant/activity/MainActivity.java
+++ Android/app/src/main/java/com/example/myrestaurant/activity/MainActivity.java
@@ -43,15 +43,13 @@ public class MainActivity extends AppCompatActivity {
 
     private GpsTracker gpsTracker;
 
-    private static final int GPS_ENABLE_REQUEST_CODE = 2001;
-    private static final int PERMISSIONS_REQUEST_CODE = 100;
-    String[] REQUIRED_PERMISSIONS = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
     String address;
 
 
     private final int searchNumber = 10;
     String[] placeData = new String[searchNumber];
     String[] linkData = new String[searchNumber];
+    BackGroundTask task;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,7 +58,6 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView = findViewById(R.id.recycler1);
         findButton = findViewById(R.id.findButton);
 
-        //gps 기반 주소값 따오는 기능
         gpsTracker = new GpsTracker(MainActivity.this);
         double latitude = gpsTracker.getLatitude();
         double longitude = gpsTracker.getLongitude();
@@ -68,6 +65,33 @@ public class MainActivity extends AppCompatActivity {
         address = addressChanger(address);
 
         Toast.makeText(getApplicationContext(), address, Toast.LENGTH_SHORT).show();
+
+        //데이터를 카드 뷰에 넣어서 카드를 만드는 기능
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        myDataset = new ArrayList<>();
+        final MyAdapter mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+
+        mAdapter.setOnItemClickListener(new MyAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View v, int position) {
+                Toast.makeText(getApplicationContext(), "Click the Button!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //예시 데이타 추가
+        myDataset.add(new MyData("", R.mipmap.spaghetti, "https://place.map.kakao.com/1492599844?service=search_pc"));
+
+        findButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                task = new BackGroundTask();
+                task.execute(100);
+
+            }
+        });
     }
 
     public String getCurrentAddress(double latitude, double longitude) {
@@ -92,7 +116,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         if (addresses == null || addresses.size() == 0) {
-            Toast.makeText(this, "주소 없음", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "주소 미발견", Toast.LENGTH_LONG).show();
             return "주소 미발견";
 
         }
@@ -102,32 +126,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    //여기부터는 GPS 활성화를 위한 메소드들
-    private void showDialogForLocationServiceSetting() {
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-        builder.setTitle("위치 서비스 비활성화");
-        builder.setMessage("앱을 사용하기 위해서는 위치 서비스가 필요합니다.\n"
-                + "위치 설정을 수정하실래요?");
-        builder.setCancelable(true);
-        builder.setPositiveButton("설정", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int id) {
-                Intent callGPSSettingIntent
-                        = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivityForResult(callGPSSettingIntent, GPS_ENABLE_REQUEST_CODE);
-            }
-        });
-        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int id) {
-                dialog.cancel();
-            }
-        });
-        builder.create().show();
-    }
-
-    //주소 값을 군, 혹은 구 단위까지만 나오도록 변환
     public String addressChanger(String address) {
         String result = "";
         int cursor = 0;
@@ -143,4 +141,58 @@ public class MainActivity extends AppCompatActivity {
         return result;
     }
 
+    class BackGroundTask extends AsyncTask<Integer, Integer, Integer> {
+
+        ProgressDialog asyncDialog;
+
+        @Override
+        protected void onPreExecute() {
+            asyncDialog = new ProgressDialog(MainActivity.this);
+            asyncDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            asyncDialog.setMessage("주변 맛집 찾는 중..");
+            asyncDialog.show();
+            super.onPreExecute();
+        }
+
+        protected Integer doInBackground(Integer... values) {
+            try {
+                getKakaoAPIData getKakaoAPIData = new getKakaoAPIData(address);
+                placeData = getKakaoAPIData.getAPIData();
+                for (int i = 0; i < 5; i++) {
+                    linkData[i] = placeData[i + searchNumber];
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return 1;
+        }
+
+        protected void onPostExecute(Integer result) {
+            asyncDialog.dismiss();
+            super.onPostExecute(result);
+
+            mLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.HORIZONTAL, false);
+            mRecyclerView.setLayoutManager(mLayoutManager);
+            myDataset = new ArrayList<>();
+            final MyAdapter mAdapter = new MyAdapter(myDataset);
+            mRecyclerView.setAdapter(mAdapter);
+
+            mAdapter.setOnItemClickListener(new MyAdapter.OnItemClickListener() {
+                @Override
+                public void onItemClick(View v, int position) {
+                    Intent intent = new Intent(getApplicationContext(), WebActivity.class);
+                    intent.putExtra("URL", linkData[position]);
+                    startActivity(intent);
+                }
+            });
+
+            for (int i = 0; i < 5; i++) {
+                myDataset.add(new MyData(placeData[i], R.mipmap.spaghetti, linkData[i]));
+            }
+
+        }
+
+    }
 }
