139a40a76c0b148510fd58cd40ddce9dc835dc66
seonb2n <626ksb@gmail.com>
2022-05-31 14:45:28 +0900

    security 삭제 및 ddd 에 맞지 않는 class 모두 삭제

--- Spring/src/main/java/com/example/myrestaurant/domain/user/domain/User.java
+++ Spring/src/main/java/com/example/myrestaurant/domain/user/domain/User.java
@@ -1,5 +1,6 @@
 package com.example.myrestaurant.domain.user.domain;
 
+import com.example.myrestaurant.domain.BaseEntity;
 import com.example.myrestaurant.domain.restaurant.domain.Restaurant;
 import lombok.*;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,24 +9,19 @@ import javax.persistence.*;
 import java.util.Collections;
 import java.util.Set;
 
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Entity
+@Getter
 @Table(name="user")
-public class User implements UserDetails {
+public class User extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 
-    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinColumn(name = "user_id", foreignKey = @ForeignKey(name = "user_id"))
-    private Set<UserAuthority> authorities;
-
-    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinColumn(name = "user_id")
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
     private Set<Restaurant> restaurantList;
 
     private String email;
@@ -35,26 +31,6 @@ public class User implements UserDetails {
     private String nickName;
 
     private boolean enabled;
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return enabled;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return enabled;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return enabled;
-    }
 
     public void addRestaurant(Restaurant... restaurants) {
         Collections.addAll(this.restaurantList, restaurants);
---
+++ Spring/src/main/java/com/example/myrestaurant/domain/user/domain/UserInfo.java
@@ -0,0 +1,4 @@
+package com.example.myrestaurant.domain.user.domain;
+
+public class UserInfo {
+}
